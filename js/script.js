a68e127263b0ee68d1b336b3276d9bd6d1b5f00f
Simon <fiewind1@gmail.com>
2021-09-08 19:29:03 +0300

    Answer to the first exercise as intendet by the teacher

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 "use strict";
 
-const numberOfFilms = prompt("How many movies have you watched?", "") ;
+const numberOfFilms = prompt("How many movies have you watched?", "");
 
 const lastWatched1 = prompt("What's the last movie you watched?", ""),
-      lastWatchedRating1 = prompt("What would you rate it?", "");
+    lastWatchedRating1 = prompt("What would you rate it?", "");
 
 const lastWatched2 = prompt("What's the second to last movie you watched?", ""),
-      lastWatchedRating2 = prompt("What would you rate it?", "");
+    lastWatchedRating2 = prompt("What would you rate it?", "");
 
-      const personalMovieDB = {
-        count: numberOfFilms,
-        movies:{lastWatched1: lastWatchedRating1,
-                lastWatched2: lastWatchedRating2
-               },
-        actors:{},
-        genres:[],
-        private: false
-    };
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+personalMovieDB.movies[lastWatched1] = [lastWatchedRating1];
+personalMovieDB.movies[lastWatched2] = [lastWatchedRating2];
+
+console.log(personalMovieDB.movies);
