83d523636f636ce2dcba259464fe7f8152b84676
Simon <fiewind1@gmail.com>
2021-09-08 19:09:48 +0300

    First exercise from the Udemy course.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 "use strict";
 
- alert("Hello World!");
+const numberOfFilms = prompt("How many movies have you watched?", "") ;
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    private: false
+};
+
+const lastWatched1 = prompt("What's the last movie you watched?", ""),
+      lastWatchedRating1 = prompt("What would you rate it?", "");
+
+const lastWatched2 = prompt("What's the second to last movie you watched?", ""),
+      lastWatchedRating2 = prompt("What would you rate it?", "");
+
+const movies = {
+    lastWatched1: lastWatchedRating1,
+    lastWatched2: lastWatchedRating2
+};
