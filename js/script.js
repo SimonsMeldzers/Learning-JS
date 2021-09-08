2f57c331e5d4beaf97ae41c32b2215484f8bd4e1
Simon <fiewind1@gmail.com>
2021-09-08 19:17:45 +0300

    Reworked the first exercise.

--- js/script.js
+++ js/script.js
@@ -2,21 +2,18 @@
 
 const numberOfFilms = prompt("How many movies have you watched?", "") ;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    private: false
-};
-
 const lastWatched1 = prompt("What's the last movie you watched?", ""),
       lastWatchedRating1 = prompt("What would you rate it?", "");
 
 const lastWatched2 = prompt("What's the second to last movie you watched?", ""),
       lastWatchedRating2 = prompt("What would you rate it?", "");
 
-const movies = {
-    lastWatched1: lastWatchedRating1,
-    lastWatched2: lastWatchedRating2
-};
+      const personalMovieDB = {
+        count: numberOfFilms,
+        movies:{lastWatched1: lastWatchedRating1,
+                lastWatched2: lastWatchedRating2
+               },
+        actors:{},
+        genres:[],
+        private: false
+    };
